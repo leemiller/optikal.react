7bb6ab1710485e93d9d707b2665f144a796ea9f1
Lee Miller <lee@c2tech.rocks>
2021-04-01 15:03:34 -0600

    add state to the App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import './App.css';
-
 import {Stage} from 'react-konva'
-
-import {Notes} from "./notes";
-import {ScaleConstellation} from "./components/ScaleConstellation";
-import {ColorWheel} from "./components/ColorWheel";
-
-const boxSideLength = 500
-const centerPoint = boxSideLength / 2
-const wedgeRadius = 150
-const labelDistanceFromCenter = wedgeRadius + 50
-const wedgeAngle = 360 / Notes.length
+import Notes from "./Notes";
+import ScaleConstellation from "./components/ScaleConstellation";
+import ColorWheel from "./components/ColorWheel";
+import React, {useMemo, useState} from "react";
+import Scales from "./Scales";
+import {FrettedInstrument} from "./components/FrettedInstrument";
+import {instrumentSettings, wheelStageSideLength} from "./settings";
+import TonicSelector from "./components/TonicSelector";
+import ScaleSelector from "./components/ScaleSelector";
+import InstrumentSelector from "./components/InstrumentSelector";
+import FlattedNameToggle from "./components/FlattedNameToggle";
 
 function App() {
+    const [tonicIndex, setTonicIndex] = useState(0)
+    const [scaleName, setScaleName] = useState('Major')
+    const [useFlats, setUseFlats] = useState(false)
+    const [activeNote, setActiveNote] = useState(null)
+    const [currentInstrument, setCurrentInstrument] = useState('guitar')
+
+    const getNoteName = useMemo(() => note => (useFlats && note.altName) || note.name, [useFlats])
+    const scale = useMemo(() => Scales[scaleName].map(ni => (ni + tonicIndex) % Notes.length), [scaleName, tonicIndex])
+
     return (
         <div className="App">
-            <Stage width={boxSideLength} height={boxSideLength}>
-                <ColorWheel wedgeAngle={wedgeAngle} centerPoint={centerPoint} wedgeRadius={wedgeRadius}
-                            labelRadius={labelDistanceFromCenter}/>
-                <ScaleConstellation scaleNoteIndexes={[0, 2, 4, 5, 7, 9, 11]} centerPoint={centerPoint}
-                                    wedgeAngle={wedgeAngle} radius={wedgeRadius}/>
+            <div id="option-selections">
+                <TonicSelector
+                    getNoteName={getNoteName}
+                    setTonicIndex={setTonicIndex}
+                    tonicIndex={tonicIndex}/>
+                <ScaleSelector
+                    setScaleName={setScaleName}
+                    scaleName={scaleName}/>
+                <InstrumentSelector
+                    setCurrentInstrument={setCurrentInstrument}
+                    currentInstrument={currentInstrument}/>
+                <FlattedNameToggle
+                    setUseFlats={setUseFlats}
+                    useFlats={useFlats}/>
+            </div>
+            <Stage width={wheelStageSideLength} height={wheelStageSideLength}>
+                <ColorWheel
+                    getNoteName={getNoteName}
+                    setActiveNote={setActiveNote}
+                    activeNote={activeNote}/>
+                <ScaleConstellation scaleNoteIndices={scale}/>
             </Stage>
+            <FrettedInstrument
+                scaleNoteIndices={scale}
+                activeNote={activeNote}
+                instrumentSettings={instrumentSettings[currentInstrument]}/>
         </div>
     );
 }
